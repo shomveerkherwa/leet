b55d80574d5b2a08704fd53f6b42b9384911d4d3
SK-aped <som.kherwa@momentoustech.com>
2021-12-07 15:42:22 +0530

    maximum array

---
+++ src/challenge/Maximum_SubArray.java
@@ -0,0 +1,16 @@
+package challenge;
+
+public class Maximum_SubArray {
+
+	public static void main(String[] args) {
+		int[] input = {-2,1,-3,4,-1,2,1,-5,4};
+		int minIndex = 0;
+		int maxIndex = 0;
+		int maxSoFar = 0;
+		int maxInLoop = 0;
+		
+		//TODO 
+		//learn kadanes algo 
+	}
+
+}
